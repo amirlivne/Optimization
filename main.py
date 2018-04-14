8f938f28438a5f0248f17117104689853d6a73f1
amirlivn <amir.livne@intel.com>
2018-04-14 20:01:25 +0300

    added task 4&5 implementation, undere main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
-from pika import *
+from hw1.pika import *
 import matplotlib.pyplot as plt
-
+from tqdm import tqdm
 
 ### task 4
 def numdiff(myfunc, x, *args):
@@ -16,7 +16,7 @@ def numdiff(myfunc, x, *args):
     """
     epsilon = args[0]
     e_mat = np.identity(len(x))
-    args = args[1:]
+    args = args[1:]  # the args to pass to the function
     # calc grad and hessian
     output_grad = np.zeros((len(x), 1))
     output_hessian = np.zeros((len(x), len(x)))
@@ -27,43 +27,57 @@ def numdiff(myfunc, x, *args):
         output_hessian[:, i] = (g_plus - g_minus) / (2 * epsilon)
     return output_grad, output_hessian
 
-
-def analyze(f1_res, f2_res, num_f1_res, num_f2_res, input):
+### task 5
+def analyze(f1_res, f2_res, numeric_list, input, epsilons):
+    num_f1_res, num_f2_res = numeric_list[3]
     values, grads, hessians = [[x, y] for x, y in zip(f1_res, f2_res)]
     num_grads, num_hessians = [[x, y] for x, y in zip(num_f1_res, num_f2_res)]
-    # value1, grad1, hessian1 = f1_res
-    # value2, grad2, hessian2 = f2_res
-    # num_grad1, num_hess1 = num_f1_res
-    # num_grad2, num_hess2 = num_f2_res
-    # grad_diff = num_grads[0].reshape(grads[0].shape) - grads[0]
+
+    grad_per_eps, hes_per_eps = [[],[]],[[],[]]
+
+    for (num1, num2) in numeric_list:
+        grad_diff1 = num1[0].reshape(grads[0].shape) - grads[0]
+        hess_diff1 = num1[1] - hessians[0]
+        grad_diff2 = num2[0].reshape(grads[1].shape) - grads[1]
+        hess_diff2 = num2[1] - hessians[1]
+        grad_per_eps[0].append(np.amax(abs(grad_diff1)))
+        grad_per_eps[1].append(np.amax(abs(grad_diff2)))
+        hes_per_eps[0].append(np.amax(abs(hess_diff1)))
+        hes_per_eps[1].append(np.amax(abs(hess_diff2)))
 
     for i in range(2):
         ## init figure
         fig = plt.figure(i + 1)
         fig.suptitle("f{} Analysis".format(i + 1), fontsize=14)
-        ax1, ax2 = fig.add_subplot(221), fig.add_subplot(222)
+        ax1, ax2, ax3, ax4 = fig.add_subplot(221), fig.add_subplot(222), fig.add_subplot(223), fig.add_subplot(224)
         grad_diff = num_grads[i].reshape(grads[0].shape) - grads[i]
         hess_diff = num_hessians[i] - hessians[i]
         ## plot grad
-        ax1.plot(input, grad_diff)
-        ax1.set_title("Gradient Difference", fontsize=10)
+        ax1.plot(input, grad_diff), ax1.set_title("Gradient Difference", fontsize=10)
         ## plot hessian
-        ax2.imshow(hess_diff, extent=[0, 1, 0, 1])
-        ax2.set_title("Hessian Difference", fontsize=10)
-
+        ax2.imshow(hess_diff, extent=[0, 1, 0, 1]), ax2.set_title("Hessian Difference", fontsize=10)
+        ## plot max grad per epsilon
+        ax3.semilogx(epsilons, grad_per_eps[i]), ax3.set_title("Max diff of gradient per epsilon", fontsize=10)
+        ## plot max grad per epsilon
+        ax4.semilogx(epsilons, hes_per_eps[i]), ax4.set_title("Max diff of hess per epsilon", fontsize=10)
         plt.show()
 
 
 if __name__ == "__main__":
-    inputs = np.random.randint(10, size=3)
+    # inputs = np.random.randint(10, size=3)
     inputs = np.array((1, 2, 3))
-    A = np.random.randn(3, 3)
-    A = np.array(([1, 2, 3], [1, 2, 3], [1, 2, 3]))
+    # A = np.random.randn(3, 3)
+    A = np.array(([1, 1, 1], [1, 1, 1], [1, 1, 1]))
     args1 = [phi, A]
     args2 = [h, phi]
-    epsilon = ((2 ** -16) ** (1 / 3)) * max(abs(inputs))
+    values = np.linspace(0, 15, 1000)
+    epsilons = [(2 ** -(16-x)) * max(abs(inputs)) for x in values]
+    print(np.amin(epsilons), np.amax(epsilons))
     f1_res = f1(inputs, *args1)
     f2_res = f2(inputs, *args2)
-    num_f1 = numdiff(f1, inputs, epsilon, *args1)
-    num_f2 = numdiff(f2, inputs, epsilon, *args2)
-    analyze(f1_res, f2_res, num_f1, num_f2, inputs)
+    numeric_list = []
+    for eps in tqdm(epsilons):
+        num_f1 = numdiff(f1, inputs, eps, *args1)
+        num_f2 = numdiff(f2, inputs, eps, *args2)
+        numeric_list.append((num_f1,num_f2))
+    analyze(f1_res, f2_res, numeric_list, inputs, epsilons)
