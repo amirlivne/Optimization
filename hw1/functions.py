95b80607cffbe6949b7fd531dfffdbcbd18abfd3
amirlivne <amirlivne2@gmail.com>
2018-04-24 14:21:03 +0300

    added nargout=2 option

--- hw1/functions.py
+++ hw1/functions.py
@@ -2,31 +2,37 @@
 """
 Created on Sun Apr  8 19:58:32 2018
 
-@author: carmelr
+@authors: carmelr and amirlivne
 """
 
 import numpy as np
 from scipy.optimize import approx_fprime
 
-
+#task 3
 # phi = sinx(x1*x2*x3)
 def phi(x, nargout=1):
     value = np.sin(np.prod(x))
-    if nargout == 3:
+    if nargout == 1:
+        return value, None, None
+    elif nargout == 2:
+        grad = np.cos(np.prod(x)) * np.array([x[1] * x[2], x[0] * x[2], x[1] * x[0]])
+        return value, grad, None
+    elif nargout == 3:
         grad = np.cos(np.prod(x)) * np.array([x[1] * x[2], x[0] * x[2], x[1] * x[0]])
         hessian = -value * (np.prod(x) ** 2 / np.outer(x, x)) + np.cos(np.prod(x)) * np.array(
             [[0, x[2], x[1]], [x[2], 0, x[0]], [x[1], x[0], 0]])
         return value, grad, hessian
-    else:
-        return value
+
 
 
 # h = exp(x)
 def h(x, nargout=1):
     if nargout == 3:
         return np.exp(x), np.exp(x), np.exp(x)
+    elif nargout == 2:
+        return np.exp(x), np.exp(x), None
     else:
-        return np.exp(x)
+        return np.exp(x), None, None
 
 
 # compute the value, grad and hessian of func(Ax)
@@ -42,14 +48,14 @@ def f1(x, **kwargs):
         func = kwargs['func']
     else:
         func = phi
-
+    value, grad, hessian = func(np.dot(A, x), nargout)
     if nargout == 3:
-        value, grad, hessian = func(np.dot(A, x), nargout)
         return value, np.dot(grad, A), np.dot(np.transpose(A), hessian).dot(A)
         # return value, np.matmul(np.transpose(A), grad), np.matmul(np.transpose(A), hessian).__matmul__(A)
-
+    elif nargout == 2:
+        return value, np.dot(grad, A)
     else:
-        return func(np.dot(A, x))
+        return value
 
 
 # compute the value, grad and hessian of scalar_func(inner_func(x))
@@ -67,14 +73,44 @@ def f2(x, **kwargs):
     else:
         inner_func = phi
 
+    in_value, in_grad, in_hessian = inner_func(x, **kwargs)
+    value, first_d, sec_d = scalar_func(in_value, nargout=nargout)
     if nargout == 3:
-        in_value, in_grad, in_hessian = inner_func(x, **kwargs)
-        value, first_d, sec_d = scalar_func(in_value, nargout=3)
         grad = first_d * in_grad
         hessian = sec_d * np.outer(in_grad, in_grad) + in_hessian * first_d
         return value, grad, hessian
+    elif nargout == 2:
+        grad = first_d * in_grad
+        return value, grad
     else:
-        return scalar_func(inner_func(x, **kwargs))
+        return value
+
+
+# task 4
+def numdiff(myfunc, x, *args):
+    """
+    :param myfunc: function handler, will contain functions from tasks 3, to
+                    obtain the function value and analytical gradient if needed
+    :param x: vector that represents the point on which we calculate the output
+    :param *args: all additional params for the calculation for example epsilon
+            (increment of x) for the following task, and for the functions
+    :return: gnum: numerical estimation of gradient.
+             Hnum: numerical estimation of hessian.
+    """
+    N = len(x)
+    epsilon = args[0]
+    e_mat = np.identity(N)
+    args = args[1]  # the args to pass to the function
+
+    # calc grad and hessian
+    output_grad = np.zeros((N, 1))
+    output_hessian = np.zeros((N, N))
+    for i in range(N):
+        val_plus, g_plus = myfunc(x + epsilon * e_mat[i], **args)
+        val_minus, g_minus = myfunc(x - epsilon * e_mat[i], **args)
+        output_grad[i] = (val_plus - val_minus) / (2 * epsilon)
+        output_hessian[:, i] = (g_plus - g_minus) / (2 * epsilon)
+    return output_grad, output_hessian
 
 
 # Hessian Matrix Aproximation
@@ -99,9 +135,3 @@ def hessian(x, the_func):
 
 # f1(a, phi, nargout=3, A=A, func=phi)
 # f2(a, scalar_func=h, inner_func=phi)
-
-# eps = np.sqrt(np.finfo(float).eps)
-# result = approx_fprime(a, phi, eps)
-#
-# value, grad, hessian2 = f1(a, nargout=3, A=A)
-# hessian_ = hessian(a, f1)
--- hw1/main.py
+++ hw1/main.py
@@ -3,36 +3,8 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-# task 4
-def numdiff(myfunc, x, *args):
-    """
-    :param myfunc: function handler, will contain functions from tasks 3, to
-                    obtain the function value and analytical gradient if needed
-    :param x: vector that represents the point on which we calculate the output
-    :param *args: all additional params for the calculation for example epsilon
-            (increment of x) for the following task, and for the functions
-    :return: gnum: numerical estimation of gradient.
-             Hnum: numerical estimation of hessian.
-    """
-    N = len(x)
-    epsilon = args[0]
-    e_mat = np.identity(N)
-    args = args[1]  # the args to pass to the function
 
-    # calc grad and hessian
-    output_grad = np.zeros((N, 1))
-    output_hessian = np.zeros((N, N))
-    for i in range(N):
-        val_plus, g_plus, _ = myfunc(x + epsilon * e_mat[i], **args)
-        val_minus, g_minus, _ = myfunc(x - epsilon * e_mat[i], **args)
-        output_grad[i] = (val_plus - val_minus) / (2 * epsilon)
-        output_hessian[:, i] = (g_plus - g_minus) / (2 * epsilon)
-
-    return output_grad, output_hessian
-
-
-# task 5
-def analyze(f1_res, f2_res, numeric_list, input, epsilons):
+def analyze(f1_res, f2_res, numeric_list, epsilons):
     num_f1_res, num_f2_res = numeric_list[0]
     values, grads, hessians = [[x, y] for x, y in zip(f1_res, f2_res)]
     num_grads, num_hessians = [[x, y] for x, y in zip(num_f1_res, num_f2_res)]
@@ -48,8 +20,6 @@ def analyze(f1_res, f2_res, numeric_list, input, epsilons):
         grad_per_eps[1].append(np.amax(abs(grad_diff2)))
         hes_per_eps[0].append(np.amax(abs(hess_diff1)))
         hes_per_eps[1].append(np.amax(abs(hess_diff2)))
-
-
 
 
     for i in range(2):
@@ -72,24 +42,20 @@ def analyze(f1_res, f2_res, numeric_list, input, epsilons):
         # plot max grad per epsilon
         plt.subplot(224), plt.loglog(epsilons[1:], hes_per_eps[i][1:]), plt.xlabel("epsilon value"), plt.ylabel("error"), plt.title("Max abs diff of hess per epsilon", fontsize=fontsize)
         plt.show()
- # plt.xlim((0, 10**-2))
 
 if __name__ == "__main__":
-    # inputs = np.random.randint(10, size=3)
     inputs = np.array((0.5377, 1.8399, -2.2588))
     A = np.array(([8, 1, 6], [3, 5, 7], [4, 9, 2]))
-    # A = np.eye(3)
-    args1 = {'func': phi, 'nargout': 3, 'A': A}
-    args2 = {'nargout':3}
-    values = np.arange(0,4,1/150)
-    original_eps = [(2 ** -(16)**1/3) * max(abs(inputs))]
-    epsilons = original_eps + [((2 ** -(16)**1/3) * max(abs(inputs)))**x for x in values]
-    # print(np.amin(epsilons), np.amax(epsilons))
-    f1_res = f1(inputs, **args1)
-    f2_res = f2(inputs, **args2)
+    args1 = {'func': phi, 'nargout': 2, 'A': A}
+    args2 = {'nargout':2}
+    values = np.arange(0, 4, 1/150)
+    original_eps = [((2 ** (-16))**1/3) * max(abs(inputs))]
+    epsilons = original_eps + [(((2 ** (-16))**1/3) * max(abs(inputs)))**x for x in values]
+    f1_res = f1(inputs, func=phi, nargout=3, A=A)
+    f2_res = f2(inputs, nargout=3)
     numeric_list = []
-    for eps in tqdm(epsilons):
+    for eps in epsilons:
         num_f1 = numdiff(f1, inputs, eps, args1)
         num_f2 = numdiff(f2, inputs, eps, args2)
         numeric_list.append((num_f1,num_f2))
-    analyze(f1_res, f2_res, numeric_list, inputs, epsilons)
+    analyze(f1_res, f2_res, numeric_list, epsilons)
